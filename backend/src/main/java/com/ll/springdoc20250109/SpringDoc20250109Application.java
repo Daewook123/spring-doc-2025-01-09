f2751bc477e0f0375d138e783f9b43c5ec62a56b
Daewook123 <cdo5080@gmail.com>
2025-01-09 14:55:22 +0900

    setting

---
+++ backend/src/main/java/com/ll/springdoc20250109/SpringDoc20250109Application.java
@@ -0,0 +1,13 @@
+package com.ll.springdoc20250109;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDoc20250109Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDoc20250109Application.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/ll/springdoc20250109/SpringDoc20250109ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.springdoc20250109;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDoc20250109ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
