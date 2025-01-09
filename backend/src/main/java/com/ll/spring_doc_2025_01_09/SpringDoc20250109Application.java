fee9a522f44f02ad577763c226f00a46879c6911
Daewook123 <cdo5080@gmail.com>
2025-01-09 15:17:59 +0900

    setting

--- backend/src/main/java/com/ll/spring_doc_2025_01_09/SpringDoc20250109Application.java
+++ backend/src/main/java/com/ll/spring_doc_2025_01_09/SpringDoc20250109Application.java
@@ -1,9 +1,11 @@
-package com.ll.springdoc20250109;
+package com.ll.spring_doc_2025_01_09;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringDoc20250109Application {
 
     public static void main(String[] args) {
